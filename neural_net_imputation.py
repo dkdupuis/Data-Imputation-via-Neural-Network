804a46b01408adacd55c589e92e0d50d671450ec
Dave <david.dupuis@hbo.com>
2020-04-26 15:04:44 -0400

    fix whitespace bug

--- neural_net_imputation.py
+++ neural_net_imputation.py
@@ -41,49 +41,49 @@ data = create_sample_data(num_movies, num_months)
 
 
 class Encoder(tf.keras.layers.Layer):
-  def __init__(self, intermediate_dim):
-    super(Encoder, self).__init__()
-    self.hidden_layer = tf.keras.layers.Dense(
-      units=intermediate_dim,
-      activation=tf.nn.relu,
-      kernel_initializer='he_uniform'
-    )
-    self.output_layer = tf.keras.layers.Dense(
-      units=intermediate_dim,
-      activation=tf.nn.sigmoid
-    )
-
-  def call(self, input_features):
-    activation = self.hidden_layer(input_features)
-    return self.output_layer(activation)
+    def __init__(self, intermediate_dim):
+        super(Encoder, self).__init__()
+        self.hidden_layer = tf.keras.layers.Dense(
+          units=intermediate_dim,
+          activation=tf.nn.relu,
+          kernel_initializer='he_uniform'
+        )
+        self.output_layer = tf.keras.layers.Dense(
+          units=intermediate_dim,
+          activation=tf.nn.sigmoid
+        )
+    
+    def call(self, input_features):
+        activation = self.hidden_layer(input_features)
+        return self.output_layer(activation)
 
 class Decoder(tf.keras.layers.Layer):
-  def __init__(self, intermediate_dim, original_dim):
-    super(Decoder, self).__init__()
-    self.hidden_layer = tf.keras.layers.Dense(
-      units=intermediate_dim,
-      activation=tf.nn.relu,
-      kernel_initializer='he_uniform'
-    )
-    self.output_layer = tf.keras.layers.Dense(
-      units=original_dim,
-      activation=tf.nn.sigmoid
-    )
-
-  def call(self, code):
-    activation = self.hidden_layer(code)
-    return self.output_layer(activation)
+    def __init__(self, intermediate_dim, original_dim):
+        super(Decoder, self).__init__()
+        self.hidden_layer = tf.keras.layers.Dense(
+          units=intermediate_dim,
+          activation=tf.nn.relu,
+          kernel_initializer='he_uniform'
+        )
+        self.output_layer = tf.keras.layers.Dense(
+          units=original_dim,
+          activation=tf.nn.sigmoid
+        )
+    
+    def call(self, code):
+        activation = self.hidden_layer(code)
+        return self.output_layer(activation)
 
 class Autoencoder(tf.keras.Model):
-  def __init__(self, intermediate_dim, original_dim):
-    super(Autoencoder, self).__init__()
-    self.encoder = Encoder(intermediate_dim=intermediate_dim)
-    self.decoder = Decoder(intermediate_dim=intermediate_dim, original_dim=original_dim)
-
-  def call(self, input_features):
-    code = self.encoder(input_features)
-    reconstructed = self.decoder(code)
-    return reconstructed
+    def __init__(self, intermediate_dim, original_dim):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder(intermediate_dim=intermediate_dim)
+        self.decoder = Decoder(intermediate_dim=intermediate_dim, original_dim=original_dim)
+    
+    def call(self, input_features):
+        code = self.encoder(input_features)
+        reconstructed = self.decoder(code)
+        return reconstructed
 
 def loss(model, original):
   reconstruction_error = tf.reduce_mean(tf.square(tf.subtract(model(original), original)))
